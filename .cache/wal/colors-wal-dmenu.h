ea40c1cd84b202b22e44d0d9674c684ce64ea668
Reep007 <bradcarcare@gmail.com>
2025-05-26 00:09:31 -0400

    updated dotfiles

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#cfdfee", "#040408" },
-	[SchemeSel] = { "#cfdfee", "#4B4A42" },
-	[SchemeOut] = { "#cfdfee", "#5DA5EA" },
-};
--- .cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#040408";
-static const char* selfgcolor   = "#cfdfee";
-static const char* normbgcolor  = "#1D639B";
-static const char* normfgcolor  = "#cfdfee";
-static const char* urgbgcolor   = "#4B4A42";
-static const char* urgfgcolor   = "#cfdfee";
